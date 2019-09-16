fd313adaa8973556d9adfdadd75bf9cc03860f3b
Srdjan Obucina <obucinac@gmail.com>
2019-09-16 21:24:25 +0200

    WIP, Moving Bitcoin listener to plugin
    - Original source code taken from https://github.com/peerplays-network/peerplays/pull/11

--- libraries/plugins/peerplays_sidechain/peerplays_sidechain_plugin.cpp
+++ libraries/plugins/peerplays_sidechain/peerplays_sidechain_plugin.cpp
@@ -1,5 +1,7 @@
 #include <graphene/peerplays_sidechain/peerplays_sidechain_plugin.hpp>
 
+#include <graphene/peerplays_sidechain/sidechain_network_manager.hpp>
+
 namespace graphene { namespace peerplays_sidechain {
 
 namespace detail
@@ -15,6 +17,10 @@ class peerplays_sidechain_plugin_impl
       virtual ~peerplays_sidechain_plugin_impl();
 
       peerplays_sidechain_plugin& _self;
+
+      sidechain::sidechain_net_manager bitcoin_manager;
+
+private:
 };
 
 peerplays_sidechain_plugin_impl::~peerplays_sidechain_plugin_impl()
@@ -40,15 +46,36 @@ std::string peerplays_sidechain_plugin::plugin_name()const
 }
 
 void peerplays_sidechain_plugin::plugin_set_program_options(
-   boost::program_options::options_description& /*cli*/,
-   boost::program_options::options_description& /*cfg*/
+   boost::program_options::options_description& cli,
+   boost::program_options::options_description& cfg
    )
 {
+   cli.add_options()
+         ("bitcoin-node-ip", bpo::value<string>()->implicit_value("127.0.0.1"), "IP address of Bitcoin node")
+         ("bitcoin-node-zmq-port", bpo::value<uint32_t>()->implicit_value(28332), "ZMQ port of Bitcoin node")
+         ("bitcoin-node-rpc-port", bpo::value<uint32_t>()->implicit_value(18332), "RPC port of Bitcoin node")
+         ("bitcoin-node-rpc-user", bpo::value<string>(), "Bitcoin RPC user")
+         ("bitcoin-node-rpc-password", bpo::value<string>(), "Bitcoin RPC password")
+         ;
+   cfg.add(cli);
 }
 
-void peerplays_sidechain_plugin::plugin_initialize(const boost::program_options::variables_map& /*options*/)
+void peerplays_sidechain_plugin::plugin_initialize(const boost::program_options::variables_map& options)
 {
    ilog("peerplays sidechain plugin:  plugin_initialize()");
+   if( options.count( "bitcoin-node-ip" ) && options.count( "bitcoin-node-zmq-port" ) && options.count( "bitcoin-node-rpc-port" )
+      && options.count( "bitcoin-node-rpc-user" ) && options.count( "bitcoin-node-rpc-password" ) )
+   {
+      const auto ip = options.at("bitcoin-node-ip").as<std::string>();
+      const auto zmq_port = options.at("bitcoin-node-zmq-port").as<uint32_t>();
+      const auto rpc_port = options.at("bitcoin-node-rpc-port").as<uint32_t>();
+      const auto rpc_user = options.at("bitcoin-node-rpc-user").as<std::string>();
+      const auto rpc_password = options.at("bitcoin-node-rpc-password").as<std::string>();
+
+      my.bitcoin_manager.initialize_manager(&database(), ip, zmq_port, rpc_port, rpc_user, rpc_password);
+   } else {
+      wlog("Haven't set up sidechain parameters");
+   }
 }
 
 void peerplays_sidechain_plugin::plugin_startup()
