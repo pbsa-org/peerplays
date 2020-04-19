9004325597032f73e3b0a9e340c435e6b0345acf
Srdjan Obucina <obucinac@gmail.com>
2020-04-19 04:40:25 +0200

    Remove logs

--- libraries/plugins/peerplays_sidechain/sidechain_net_handler.cpp
+++ libraries/plugins/peerplays_sidechain/sidechain_net_handler.cpp
@@ -314,16 +314,10 @@ void sidechain_net_handler::process_proposals() {
          }
 
          if (should_process) {
-            //ilog("Proposal ${po} will be processed by sidechain handler ${sidechain}", ("po", (*po).id)("sidechain", sidechain));
             bool should_approve = process_proposal(*po);
             if (should_approve) {
-               //ilog("Proposal ${po} will be approved", ("po", (*po).id));
                approve_proposal(po->id, plugin.get_current_son_id());
-            } else {
-               //ilog("Proposal ${po} is not approved", ("po", (*po).id));
             }
-         } else {
-            //ilog("Proposal ${po} will not be processed by sidechain handler ${sidechain}", ("po", (*po).id)("sidechain", sidechain));
          }
       }
    }
