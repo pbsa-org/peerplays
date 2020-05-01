37dd5571b35cf52754cf314fe0d9ab7afd7c3777
Koneru Satyanarayana <15652887+satyakoneru@users.noreply.github.com>
2020-05-01 16:21:34 +1000

    [SON-363] - Remove son deletion

--- libraries/chain/db_notify.cpp
+++ libraries/chain/db_notify.cpp
@@ -299,7 +299,7 @@ struct get_impacted_account_visitor
    void operator()( const son_update_operation& op ) {
        _impacted.insert( op.owner_account );
    }
-   void operator()( const son_delete_operation& op ) {
+   void operator()( const son_deregister_operation& op ) {
       _impacted.insert( op.owner_account );
    }
    void operator()( const son_heartbeat_operation& op ) {
