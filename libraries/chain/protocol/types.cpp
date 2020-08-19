cc77a088915d0a22160347ea21b285ee3093f31b
Nathan Hourt <nathan@followmyvote.com>
2020-08-18 19:07:16 -0500

    Fixes
    Add back in some of Peter's fixes that got lost in the cherry pick

--- libraries/chain/protocol/types.cpp
+++ libraries/chain/protocol/types.cpp
@@ -70,7 +70,7 @@ namespace graphene { namespace chain {
        auto bin = fc::from_base58( base58str.substr( prefix_len ) );
        auto bin_key = fc::raw::unpack<binary_key>(bin);
        key_data = bin_key.data;
-       FC_ASSERT( fc::ripemd160::hash( key_data.data, key_data.size() )._hash[0] == bin_key.check );
+       FC_ASSERT( fc::ripemd160::hash( (char*)key_data.data(), key_data.size() )._hash[0] == bin_key.check );
     };
 
     bool public_key_type::is_valid_muse( const std::string& base58str )
@@ -83,7 +83,7 @@ namespace graphene { namespace chain {
        auto bin = fc::from_base58( base58str.substr( prefix_len ) );
        auto bin_key = fc::raw::unpack<binary_key>(bin);
        key_data = bin_key.data;
-       FC_ASSERT( fc::ripemd160::hash( key_data.data, key_data.size() )._hash[0] == bin_key.check );
+       FC_ASSERT( fc::ripemd160::hash( (char*)key_data.data(), key_data.size() )._hash[0] == bin_key.check );
        return true;
     }
 
@@ -97,7 +97,7 @@ namespace graphene { namespace chain {
        auto bin = fc::from_base58( base58str.substr( prefix_len ) );
        auto bin_key = fc::raw::unpack<binary_key>(bin);
        fc::ecc::public_key_data key_data = bin_key.data;
-       FC_ASSERT( fc::ripemd160::hash( key_data.data, key_data.size() )._hash[0] == bin_key.check );
+       FC_ASSERT( fc::ripemd160::hash( (char*)key_data.data(), key_data.size() )._hash[0] == bin_key.check );
        return true;
     }
 
@@ -115,7 +115,7 @@ namespace graphene { namespace chain {
     {
        binary_key k;
        k.data = key_data;
-       k.check = fc::ripemd160::hash( k.data.data, k.data.size() )._hash[0];
+       k.check = fc::ripemd160::hash( (char*)k.data.data(), k.data.size() )._hash[0];
        auto data = fc::raw::pack( k );
        return GRAPHENE_ADDRESS_PREFIX + fc::to_base58( data.data(), data.size() );
     }
@@ -156,7 +156,7 @@ namespace graphene { namespace chain {
        FC_ASSERT( base58str.substr( 0, prefix_len ) ==  prefix , "", ("base58str", base58str) );
        auto bin = fc::from_base58( base58str.substr( prefix_len ) );
        auto bin_key = fc::raw::unpack<binary_key>(bin);
-       FC_ASSERT( fc::ripemd160::hash( bin_key.data.data, bin_key.data.size() )._hash[0] == bin_key.check );
+       FC_ASSERT( fc::ripemd160::hash( (char*)bin_key.data.data(), bin_key.data.size() )._hash[0] == bin_key.check );
        key_data = bin_key.data;
     }
 
@@ -169,7 +169,7 @@ namespace graphene { namespace chain {
     {
        binary_key k;
        k.data = key_data;
-       k.check = fc::ripemd160::hash( k.data.data, k.data.size() )._hash[0];
+       k.check = fc::ripemd160::hash( (char*)k.data.data(), k.data.size() )._hash[0];
        auto data = fc::raw::pack( k );
        return GRAPHENE_ADDRESS_PREFIX + fc::to_base58( data.data(), data.size() );
     }
@@ -210,7 +210,7 @@ namespace graphene { namespace chain {
        FC_ASSERT( base58str.substr( 0, prefix_len ) ==  prefix , "", ("base58str", base58str) );
        auto bin = fc::from_base58( base58str.substr( prefix_len ) );
        auto bin_key = fc::raw::unpack<binary_key>(bin);
-       FC_ASSERT( fc::ripemd160::hash( bin_key.data.data, bin_key.data.size() )._hash[0] == bin_key.check );
+       FC_ASSERT( fc::ripemd160::hash( (char*)bin_key.data.data(), bin_key.data.size() )._hash[0] == bin_key.check );
        key_data = bin_key.data;
     }
     
@@ -223,7 +223,7 @@ namespace graphene { namespace chain {
     {
        binary_key k;
        k.data = key_data;
-       k.check = fc::ripemd160::hash( k.data.data, k.data.size() )._hash[0];
+       k.check = fc::ripemd160::hash( (char*)k.data.data(), k.data.size() )._hash[0];
        auto data = fc::raw::pack( k );
        return GRAPHENE_ADDRESS_PREFIX + fc::to_base58( data.data(), data.size() );
     }
