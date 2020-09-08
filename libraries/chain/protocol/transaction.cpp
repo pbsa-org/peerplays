df7f9c3783f1a7cf04089007fd8ec21ff934fe8b
sierra19XX <15652887+sierra19XX@users.noreply.github.com>
2020-09-08 11:51:00 +0000

    compilation fix

--- libraries/chain/protocol/transaction.cpp
+++ libraries/chain/protocol/transaction.cpp
@@ -426,7 +426,7 @@ set<public_key_type> signed_transaction::minimize_required_signatures(
       result.erase( k );
       try
       {
-         graphene::chain::verify_authority( operations, result, get_active, get_owner, get_custom
+         graphene::chain::verify_authority( operations, result, get_active, get_owner, get_custom,
                                             ignore_custom_operation_required_auths, max_recursion );
          continue;  // element stays erased if verify_authority is ok
       }
